a8f855b45b042dda5c92e6a1236bd1d28ddf66cf
Yeni Hernandez <yeni.hernandez@udea.edu.co>
2023-09-25 17:17:23 -0500

    primer fecth para recuperar un hecho aleatorio

--- App.jsx
+++ App.jsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from 'react'
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
 export function App () {
+  const [fact, setFact] = useState()
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(rest => rest.json())
+      .then(data => setFact(data.fact))
+  }, [])
   return (
-    <h1>App de gaticos</h1>
+    <>
+      <h1>Aplicacion de gaticos</h1>
+      {fact && <p>{fact}</p>}
+    </>
   )
 }
