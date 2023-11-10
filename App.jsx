c923cb13934cbef372e7d06a44d296a72a440b84
Yeni Hernandez <yeni.hernandez@udea.edu.co>
2023-11-10 17:07:14 -0500

    Creación de estilos con Flex y separación de UseEfect

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
+import './app.css'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-const urlsite = 'https://cataas.com'
+const urlsite = 'https://cataas.com/says/'
 
 export function App () {
   const [fact, setFact] = useState()
@@ -13,28 +14,42 @@ export function App () {
       .then(data => {
         const { fact } = data // destructuración
         setFact(fact)
-        const SplitThreeFirstWords = fact.split(' ', 3)
-        setthreeFirstWords(SplitThreeFirstWords)
-        fetch(`https://cataas.com/cat/says/${SplitThreeFirstWords}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response      
-            setImageUrl(url)
-            console.log(url) //asegurar lo que esta llegando
-          })
+       
       })         
   }, [])
+  useEffect(() =>{
+    if(!fact) return
+    const SplitThreeFirstWords = fact.split(' ', 3)
+    setthreeFirstWords(SplitThreeFirstWords)
+    fetch(`https://cataas.com/cat/says/${SplitThreeFirstWords}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response  
+        console.log(response)    
+        setImageUrl(url)
+        console.log(url) //asegurar lo que esta llegando
+      })
+  },[fact])
 
   return (
     <> 
-      <h1>Aplicacion de gaticos</h1>
-      {/* Renderizado condicional */}
-      <h3>Recuperación del hecho:</h3>
-      {fact && <p>{fact}</p>} 
-      <h3>Tres primeras palabras del hecho:</h3>
-      {fact && <p>{threeFirstWords}</p>} 
-      <h3>Imagen relacionada al hecho:</h3>
-      {ImageUrl && <img src={`${urlsite}${ImageUrl}`} alt={`Imagen tomada desde ${fact}`} />} 
+      <main>
+        <h1 >Aplicacion de gaticos</h1>
+        {/* Renderizado condicional */}
+        <section>
+          <h3>Recuperación del hecho:</h3>
+          {fact && <p>{fact}</p>} 
+          </section>
+          <div className='palabras'>
+          <h3>Tres primeras palabras del hecho:</h3>
+          {fact && <p>{threeFirstWords}</p>} 
+          </div>
+          <div className='imagen'>            
+          <h3>Imagen relacionada al hecho:</h3>
+          {ImageUrl && <img src={`${urlsite}${ImageUrl}`} alt={`Imagen tomada desde ${fact}`} />} 
+          </div>      
+      </main>
+
     </>
   )
 }
